065907a1951d444f7f79a4ea0a1628ed3ad847b2
Kai Gowers <kai.gowers@gmail.com>
2026-09-18 16:37:41 -0700

    get_scores now returns the most recent 100 scores even when the score isn't present, and sets a score of 0 if that is the case

--- backend/app/routers/scores.py
+++ backend/app/routers/scores.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from ..security import get_current_user
 from ..models import User
 from ..schemas import ScoreResponse
+from datetime import date, timedelta
 
 
 router = APIRouter(
@@ -14,19 +15,39 @@ def get_scores(
     current_user: User = Depends(get_current_user)
 ) -> list[ScoreResponse]:
 
-    sorted_scores = sorted(
-        current_user.scores,
-        key=lambda score: score.date
-    )[-100:]
+    today = date.today()
 
-    return [
-        ScoreResponse(
-            id=score.id,
-            score=score.score,
-            date=score.date,
-        )
-        for score in sorted_scores
-    ]
+    score_by_date = {
+        score.date: score
+        for score in current_user.scores
+    }
+
+    responses = []
+
+    for days_ago in range(99, -1, -1):
+        
+        current_date = today - timedelta(days=days_ago)
+
+        existing_score = score_by_date.get(current_date)
+
+        if existing_score:
+            responses.append(
+                ScoreResponse(
+                    id=existing_score.id,
+                    score=existing_score.score,
+                    date=current_date,
+                )
+            )
+        else:
+            responses.append(
+                ScoreResponse(
+                    id=None,
+                    score=0,
+                    date=current_date,
+                )
+            )
+    
+    return responses
 
 
     
