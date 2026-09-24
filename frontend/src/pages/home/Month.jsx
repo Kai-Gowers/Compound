5b0cb87943e42ec4ceefe9be18e6ab22c9296c97
Kai Gowers <kai.gowers@gmail.com>
2026-09-24 01:20:08 -0700

    made the title more readable, with letters as the month

--- frontend/src/pages/home/Month.jsx
+++ frontend/src/pages/home/Month.jsx
@@ -4,6 +4,21 @@ import './Month.css';
 
 export function Month({ month, monthScores }) {
 
+    const numToMonth = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+
     return (
 
         <div className="score-grid">
@@ -15,7 +30,7 @@ export function Month({ month, monthScores }) {
                     <GridUnit 
                         key={monthScore.id} 
                         score={monthScore.score}
-                        title={`${monthScore.date}: ${monthScore.score * 100}`}
+                        title={`${numToMonth[Number(monthScore.date.slice(5, 7))]} ${Number(monthScore.date.slice(8, 10))}: ${monthScore.score * 100}`}
                     />
                 );
             })}
