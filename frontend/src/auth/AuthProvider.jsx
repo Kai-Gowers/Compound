b0e6abe857cac1ee1cbb35e864f2bdfb85801f8c
Kai Gowers <kai.gowers@gmail.com>
2026-09-25 23:51:31 -0700

    small formatting changes

--- frontend/src/auth/AuthProvider.jsx
+++ frontend/src/auth/AuthProvider.jsx
@@ -20,7 +20,7 @@ export function AuthProvider({ children }) {
     }, []);
 
     return (
-        <AuthContext.Provider value={{user, setUser }}>
+        <AuthContext.Provider value={{ user, setUser }}>
             {children}
         </AuthContext.Provider>
     );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import { AuthProvider } from './auth/AuthProvider.jsx';
 
 import './index.css';
 import App from './App.jsx';
-import { AuthProvider } from './auth/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
